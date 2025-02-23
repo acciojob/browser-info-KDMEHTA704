28cb592d23ce49ed772fdb58b67fabd8f6fbfcd3
KDMEHTA704 <131295594+KDMEHTA704@users.noreply.github.com>
2025-02-23 22:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const browserInfoDiv = document.createElement("div");
+    browserInfoDiv.id = "browser-info";
+    
+    const userAgent = navigator.userAgent;
+    const appName = navigator.appName;
+    const appVersion = navigator.appVersion;
+    
+    const browserInfo = `You are using ${appName} version ${appVersion}`;
+    
+    browserInfoDiv.textContent = browserInfo;
+    document.body.appendChild(browserInfoDiv);
+});
